dc9482ed41369dd644cc362c28ab775d3c0e0a68
Karla Sai Krishna Reddy <karla.s@thoughtworks.com>
2022-06-13 11:22:52 +0530

    refactors greet method and punctuation in the greet method

--- assignment1/greeting-project/src/main/java/Greeting.java
+++ assignment1/greeting-project/src/main/java/Greeting.java
@@ -10,19 +10,18 @@ public class Greeting {
     }
 
     public String greet() {
-        if (names == null) greeting.append("Hello, my friend");
-        if (names != null && names.length == 1) {
+        if (names == null) greeting.append("Hello, my friend.");
+        else if (names.length == 1) {
             if (names[0].equals(names[0].toUpperCase())) greeting.append("HELLO ").append(names[0]).append("!");
-            else greeting.append("Hello, ").append(names[0]);
-        }
-        if (names != null && names.length == 2) {
-            greeting.append("Hello, ").append(names[0]).append(" and ").append(names[1]);
+            else greeting.append("Hello, ").append(names[0]).append(".");
+        } else if (names.length == 2) {
+            greeting.append("Hello, ").append(names[0]).append(" and ").append(names[1]).append(".");
         } else {
             greeting.append("Hello, ");
             for (int i = 0; i < Objects.requireNonNull(names).length - 1; i++) {
                 greeting.append(names[i]).append(", ");
             }
-            greeting.append("and ").append(names[names.length - 1]);
+            greeting.append("and ").append(names[names.length - 1]).append(".");
         }
         return greeting.toString();
     }
